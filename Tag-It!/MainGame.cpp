1648879ec56c0e2ab83dc1438407e4799e94653e
Unknown <amegilham@gmail.com>
2020-02-29 00:01:22 -0500

    Overhaul finished
    All the motion bugs fixed, and gameplay code moved to scenes so we can easily implement more scenes (such as a main menu) in the future

--- Tag-It!/MainGame.cpp
+++ Tag-It!/MainGame.cpp
@@ -4,7 +4,7 @@
 MainGame::MainGame(std::string name)
 	: Scene(name)
 {
-	m_gravity = b2Vec2(float32(0.f), float32(0.f));
+	m_gravity = b2Vec2(float32(0.f), float32(-150.f));
 	m_physicsWorld->SetGravity(m_gravity);
 	m_physicsWorld->SetContactListener(&listener);
 }
@@ -33,6 +33,8 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 		vec4 temp = ECS::GetComponent<Camera>(entity).GetOrthoSize();
 		ECS::GetComponent<Camera>(entity).SetWindowSize(vec2(float(windowWidth), float(windowHeight)));
 		ECS::GetComponent<Camera>(entity).Orthographic(aspectRatio, temp.x, temp.y, temp.z, temp.w, -100.f, 100.f);
+		ECS::GetComponent<Camera>(entity).Zoom(75.f);
+		vec4 size = ECS::GetComponent<Camera>(entity).GetOrthoSize();
 
 		//Sets up the Identifier
 		unsigned int bitHolder = EntityIdentifier::CameraBit();
@@ -64,8 +66,8 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 			animController.InitUVs(fileName);
 
 			//setup the sprite and transform compoments 
-			ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 24, 24, true, &animController);
-			ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 10.f, 99.f+0.1f*i));
+			ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 6, 6, true, &animController);
+			ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 2.5f, 99.f+0.1f*i));
 
 			//grab references to the sprite and physic body compoments
 			auto& tempSpr = ECS::GetComponent<Sprite>(entity);
@@ -80,8 +82,8 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 			b2BodyDef tempDef;
 			tempDef.type = b2_dynamicBody;
 			//set the position
-			if (i == 0) tempDef.position.Set(float32(-72.f), float32(-30.f)); //blue position
-			else tempDef.position.Set(float32(72), float32(-30.f)); //orange position
+			if (i == 0) tempDef.position.Set(float32(-18.f), float32(-7.5f)); //blue position
+			else tempDef.position.Set(float32(18.f), float32(-7.5f)); //orange position
 
 			//add the physics body to box2D physics world simulator
 			tempBody = m_physicsWorld->CreateBody(&tempDef);
@@ -136,7 +138,7 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 
 		//loadsprite sheet and set up sprite component
 		std::string fileName = "BG.png";
-		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 615, 228);
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 154, 57);
 		//setup transform component
 		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 0.f, -10.f));
 
@@ -153,7 +155,7 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 		b2BodyDef tempDef;
 		tempDef.type = b2_staticBody;
 		//set the position
-		tempDef.position.Set(float32(0.f), float32(-30.f));
+		tempDef.position.Set(float32(0.f), float32(-7.5f));
 
 		//add the physics body to box2D physics world simulator
 		tempBody = m_physicsWorld->CreateBody(&tempDef);
@@ -182,9 +184,9 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 
 			//loadsprite sheet and set up sprite component
 			std::string fileName = "BG.png";
-			ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 30, 15);
+			ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 8, 4);
 			//setup transform component
-			ECS::GetComponent<Transform>(entity).SetPosition(vec3(10.f, 0.f, -10.f));
+			ECS::GetComponent<Transform>(entity).SetPosition(vec3(2.5f, 0.f, -10.f));
 
 			//grab references to the sprite and physics body components
 			auto& tempSpr = ECS::GetComponent<Sprite>(entity);
@@ -199,14 +201,14 @@ void MainGame::InitScene(float windowWidth, float windowHeight)
 			b2BodyDef tempDef;
 			tempDef.type = b2_staticBody;
 			//Sets positions for each platform 
-			if(i ==0 )tempDef.position.Set(float32(-145.f), float32(-20.f)); //bottom left
-			else if(i == 1)tempDef.position.Set(float32(0.f), float32(-20.f)); //bottom middle
-			else if (i == 2)tempDef.position.Set(float32(145.f), float32(-20.f)); //bottom right
-			else if (i == 3)tempDef.position.Set(float32(-72.f), float32(22.f)); //middle left
-			else if (i == 4)tempDef.position.Set(float32(72.f), float32(22.f)); //midle right
-			else if (i == 5)tempDef.position.Set(float32(-145.f), float32(60.f)); //top left
-			else if (i == 6)tempDef.position.Set(float32(0.f), float32(60.f)); //top middle
-			else if (i == 7)tempDef.position.Set(float32(145.f), float32(60.f));//top right
+			if(i ==0 )tempDef.position.Set(float32(-36.25f), float32(-5.f)); //bottom left
+			else if(i == 1)tempDef.position.Set(float32(0.f), float32(-5.f)); //bottom middle
+			else if (i == 2)tempDef.position.Set(float32(36.25f), float32(-5.f)); //bottom right
+			else if (i == 3)tempDef.position.Set(float32(-18.f), float32(5.5f)); //middle left
+			else if (i == 4)tempDef.position.Set(float32(18.f), float32(5.5f)); //midle right
+			else if (i == 5)tempDef.position.Set(float32(-36.25f), float32(15.f)); //top left
+			else if (i == 6)tempDef.position.Set(float32(0.f), float32(15.f)); //top middle
+			else if (i == 7)tempDef.position.Set(float32(36.25f), float32(15.f));//top right
 
 			//add the physics body to box2D physics world simulator
 			tempBody = m_physicsWorld->CreateBody(&tempDef);
@@ -244,12 +246,12 @@ void MainGame::Update()
 	b2Body* bluebody = bluetempPhysBod.GetBody();
 
 	//if Blue has run off the right of the screen, make her appear on the left
-	if (bluetempPhysBod.GetPosition().x > 202) {
-		bluetempPhysBod.GetBody()->SetTransform(b2Vec2(-202, bluebody->GetPosition().y), float32(0));
+	if (bluetempPhysBod.GetPosition().x > 50.5) {
+		bluetempPhysBod.GetBody()->SetTransform(b2Vec2(-50.5, bluebody->GetPosition().y), float32(0));
 	}
 	//if Blue has run off the left of the screen, make her appear on the right
-	else if (bluetempPhysBod.GetPosition().x < -202) {
-		bluetempPhysBod.GetBody()->SetTransform(b2Vec2(202, bluebody->GetPosition().y), float32(0));
+	else if (bluetempPhysBod.GetPosition().x < -50.5) {
+		bluetempPhysBod.GetBody()->SetTransform(b2Vec2(50.5, bluebody->GetPosition().y), float32(0));
 	}
 
 	//grab orange's physics body info
@@ -257,18 +259,14 @@ void MainGame::Update()
 	b2Body* orangebody = orangetempPhysBod.GetBody();
 
 	//if Orange has run off the right of the screen, make him appear on the left 
-	if (orangetempPhysBod.GetPosition().x > 202) {
-		orangetempPhysBod.GetBody()->SetTransform(b2Vec2(-202, orangebody->GetPosition().y), float32(0));
+	if (orangetempPhysBod.GetPosition().x > 50.5) {
+		orangetempPhysBod.GetBody()->SetTransform(b2Vec2(-50.5, orangebody->GetPosition().y), float32(0));
 	}
 	//if Orange has run off the left of the screen, make him appear on the right
-	else if (orangetempPhysBod.GetPosition().x < -202) {
-		orangetempPhysBod.GetBody()->SetTransform(b2Vec2(202, orangebody->GetPosition().y), float32(0));
+	else if (orangetempPhysBod.GetPosition().x < -50.5) {
+		orangetempPhysBod.GetBody()->SetTransform(b2Vec2(50.5, orangebody->GetPosition().y), float32(0));
 	}
-
-	//apply some gravity for blue and orange 
-	bluebody->SetLinearVelocity(b2Vec2(bluebody->GetLinearVelocity().x, bluebody->GetLinearVelocity().y - (9075 * (Timer::deltaTime * Timer::deltaTime))));
-	orangebody->SetLinearVelocity(b2Vec2(orangebody->GetLinearVelocity().x, orangebody->GetLinearVelocity().y - (9075 * (Timer::deltaTime * Timer::deltaTime))));
-	//printf("y velo: %f\n\n", bluebody->GetLinearVelocity().y);
+	
 }
 
 //Stroke of the gamepad input
@@ -289,138 +287,125 @@ void MainGame::GamepadTrigger(XInputController* con)
 //keyboard key held down input
 void MainGame::KeyboardHold()
 {
-	vec3 force = vec3(15000.f, 0.f, 0.f);
-	vec3 force2 = vec3(0.f, 3000.f, 0.f);
+	//vector with the force for the player's x movement
+	vec3 runforce = vec3(2000.f, 0.f, 0.f);
 
 	//grab a reference to blue's physics body
-	auto& tempPhysBod = ECS::GetComponent<PhysicsBody>(EntityIdentifier::MainPlayer());
-	//create a pointer to blue's box2d body
-	b2Body* bluebody = tempPhysBod.GetBody();
-	//create a copy of blue's velocity
-	b2Vec2 bluevel = bluebody->GetLinearVelocity();
+	auto& tempPhysBodB = ECS::GetComponent<PhysicsBody>(EntityIdentifier::MainPlayer());
+	//create a pointer to Blue's box2d body
+	b2Body* bodyB = tempPhysBodB.GetBody();
 
-	//if blue's player is pressing A, and blue's x velocity greater than -120, subtract 30 from blue's x velocity	
-	if (Input::GetKey(Key::A) && bluevel.x > float32(-120.f)) {
-		bluevel.x -= 30.f;
-		bluebody->SetLinearVelocity(bluevel);
+	//if Blue's player is pressing A, and their x-velocity isn't above the left cap, apply the run force to the left
+	if (Input::GetKey(Key::A) && bodyB->GetLinearVelocity().x > float32(-40.f)) {
+		tempPhysBodB.ApplyForce(-runforce);
 	}
 
-	//if blue's player is pressing D, and blue's x velocity less than 120, add 30 to blue's x velocity
-	else if (Input::GetKey(Key::D) && bluebody->GetLinearVelocity().x < float32(120.f)) {
-		bluevel.x += 30.f;
-		bluebody->SetLinearVelocity(bluevel);
+	//if Blue's player is pressing D, and their x-velocity isn't above the right cap, apply the run force to the right
+	else if (Input::GetKey(Key::D) && bodyB->GetLinearVelocity().x < float32(40.f)) {
+		tempPhysBodB.ApplyForce(runforce);
 	}
 
-	//otherwise, blue isn't trying to move left or right
+	//otherwise blue isn't moving on the x-axis
 	else {
-		//if blue is moving right 
-		if (bluevel.x > float32(0.f))
+		//so if she's still moving right, subtract from velocity from her motion
+		if (bodyB->GetLinearVelocity().x > float32(0.f))
 		{
-			//if her x velocity is greater than 30, subtract 30 from it
-			if (bluevel.x > float32(30.f)) {
-				bluevel = b2Vec2(bluevel.x - 30, bluevel.y);
-				bluebody->SetLinearVelocity(bluevel);
+			//if her velocity is above 5, subtract 5
+			if (bodyB->GetLinearVelocity().x > float32(5.f)) {
+
+				bodyB->SetLinearVelocity(b2Vec2(bodyB->GetLinearVelocity().x - 5, bodyB->GetLinearVelocity().y));
 			}
-			//otherwise it must be between 30 and 0, so just set it to 0
+			//otherwise it's between 0 and 5, so just set it to 0
 			else {
-				bluevel = b2Vec2(0.f, bluevel.y);
-				bluebody->SetLinearVelocity(bluevel);
+				bodyB->SetLinearVelocity(b2Vec2(0, bodyB->GetLinearVelocity().y));
 			}
 		}
-		//or if blue is moving left
-		else if (bluevel.x < float32(0.f))
+		//and if she's still moving left, add velocity to her motion (bringing her to 0 and thus not moving)
+		else if (bodyB->GetLinearVelocity().x < float32(0.f))
 		{
-			//if her x velocity is less than -30, add 30 to it 
-			if (bluevel.x < float32(-30.f)) {
+			//if her velocity is below -5, add 5
+			if (bodyB->GetLinearVelocity().x < float32(-5.f)) {
 
-				bluevel = b2Vec2(bluevel.x + 30, bluevel.y);
-				bluebody->SetLinearVelocity(bluevel);
+				bodyB->SetLinearVelocity(b2Vec2(bodyB->GetLinearVelocity().x + 5, bodyB->GetLinearVelocity().y));
 			}
-			//otherwise it must be between -30 and 0, so just set it to 0
+			//otherwise it's between -5 and 0, so just set it to 0
 			else {
-				bluevel = b2Vec2(0, bluevel.y);
-				bluebody->SetLinearVelocity(bluevel);
+				bodyB->SetLinearVelocity(b2Vec2(0, bodyB->GetLinearVelocity().y));
 			}
 		}
 	}
 
 	//grab a reference to orange's physics body
 	auto& tempPhysBodO = ECS::GetComponent<PhysicsBody>(EntityIdentifier::SecondPlayer());
-	//create a pointer to orange's box2d body
+	//create a pointer to Orange's box2d body
 	b2Body* bodyO = tempPhysBodO.GetBody();
-	//create a copy of orange's velocity
-	b2Vec2 orangevel = bodyO->GetLinearVelocity();
 
-	//if orange's player is pressing leftArrow, and orange's x velocity greater than -120, subtract 30 from orange's x velocity
-	if (Input::GetKey(Key::LeftArrow) && orangevel.x > float32(-120.f)) {
-		orangevel = b2Vec2(orangevel.x - 30, orangevel.y);
-		bodyO->SetLinearVelocity(orangevel);
+	//if Orange's player is pressing leftArrow, and their x-velocity isn't above the left cap, apply the run force to the left
+	if (Input::GetKey(Key::LeftArrow) && bodyO->GetLinearVelocity().x > float32(-40.f)) {
+		tempPhysBodO.ApplyForce(-runforce);
 	}
 
-	//if orange's player is pressing rightArrow, and orange's x velocity less than 120, add 30 to orange's x velocity
-	else if (Input::GetKey(Key::RightArrow) && orangevel.x < float32(120.f)) {
-		orangevel = b2Vec2(orangevel.x + 30, orangevel.y);
-		bodyO->SetLinearVelocity(orangevel);
+	//if Orange's player is pressing rightArrow, and their x-velocity isn't above the right cap, apply the run force to the right
+	else if (Input::GetKey(Key::RightArrow) && bodyO->GetLinearVelocity().x < float32(40.f)) {
+		tempPhysBodO.ApplyForce(runforce);
 	}
 
-	//otherwise, orange isn't trying to move left or right
+	//otherwise Orange isn't moving on the x-axis
 	else {
-		//if he is moving right 
-		if (orangevel.x > float32(0.f))
+		//so if he's still moving right, subtract from velocity from his motion
+		if (bodyO->GetLinearVelocity().x > float32(0.f))
 		{
-			//and his x velocity is greater than 30, subtract 30 from it 
-			if (orangevel.x > float32(30.f)) {
-				orangevel = b2Vec2(orangevel.x - 30, orangevel.y);
-				bodyO->SetLinearVelocity(orangevel);
+			//if his velocity is above 5, subtract 5
+			if (bodyO->GetLinearVelocity().x > float32(5.f)) {
+
+				bodyO->SetLinearVelocity(b2Vec2(bodyO->GetLinearVelocity().x - 5, bodyO->GetLinearVelocity().y));
 			}
-			//otherwise it must be between 0 and 30, so just set it to 0
+			//otherwise it's between 0 and 5, so just set it to 0
 			else {
-				orangevel = b2Vec2(0.f, orangevel.y);
-				bodyO->SetLinearVelocity(orangevel);
+				bodyO->SetLinearVelocity(b2Vec2(0, bodyO->GetLinearVelocity().y));
 			}
 		}
-		//otherwise, if he's moving left
-		else if (orangevel.x < float32(0.f))
+		//and if he's still moving left, add velocity to his motion (bringing him to 0 and thus not moving)
+		else if (bodyO->GetLinearVelocity().x < float32(0.f))
 		{
-			//and his x velocity is less than -30, add 20 to it 
-			if (orangevel.x < float32(-30.f)) {
+			//if his velocity is below -5, add 5
+			if (bodyO->GetLinearVelocity().x < float32(-5.f)) {
 
-				orangevel = b2Vec2(orangevel.x + 30, orangevel.y);
-				bodyO->SetLinearVelocity(orangevel);
+				bodyO->SetLinearVelocity(b2Vec2(bodyO->GetLinearVelocity().x + 5, bodyO->GetLinearVelocity().y));
 			}
-			//otherwise it must be between 0 and -30, so just set it to 0
+			//otherwise it's between -5 and 0, so just set it to 0
 			else {
-				orangevel = b2Vec2(0.f, orangevel.y);
-				bodyO->SetLinearVelocity(orangevel);
+				bodyO->SetLinearVelocity(b2Vec2(0, bodyO->GetLinearVelocity().y));
 			}
 		}
 	}
 
-	vec3 jump = vec3(0.f, 180000.f, 0.f);
+	//vector for the force of player's jumping
+	vec3 jump = vec3(0.f, 5750.f, 0.f);
 
+	//if blue's player has pressed W, and she can jump, make her jump
 	if (Input::GetKey(Key::W)) {
 		//Check if Blue can jump 
-		if (listener.getJumpB() && blueTimeSinceLastJump > 0.5f) {
+		if (listener.getJumpB() && blueTimeSinceLastJump > 0.4f) {
 			//if she can, set it so she can't 
 			listener.setJumpB(false);
 			blueTimeSinceLastJump = 0.f;
 			//and apply the upward force of the jump
-			bluebody->SetLinearVelocity(b2Vec2(bluevel.x, 110.f));
+			tempPhysBodB.ApplyForce(jump);
 		}
 	}
 
-	//Check if Orange can jump
+	//if orange's player has pressed upArrow, and he can jump, make him jump
 	if (Input::GetKey(Key::UpArrow)) {
-		if (listener.getJumpO() && orangeTimeSinceLastJump > 0.5f) {
+		//Check if Orange can jump 
+		if (listener.getJumpO() && orangeTimeSinceLastJump > 0.4f) {
 			//if he can, set it so he can't 
 			listener.setJumpO(false);
 			orangeTimeSinceLastJump = 0.f;
 			//and apply the upward force of the jump
-			bodyO->SetLinearVelocity(b2Vec2(orangevel.x, 110.f));
+			tempPhysBodO.ApplyForce(jump);
 		}
 	}
-
-
 }
 
 //keyboard key first pressed input
